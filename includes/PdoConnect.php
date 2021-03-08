1ecaec6d05c0c9a5909dbbd2fbeb163a765f9e15
JEAN PAUL NSHIMIYIMANA <nshjpaul@gmail.com>
2021-03-08 17:56:53 +0200

    credentials removed

--- includes/PdoConnect.php
+++ includes/PdoConnect.php
@@ -1,6 +1,6 @@
 
 <?php
-	$dsn = 'mysql: host=localhost:3306; dbname=cspchukc_tntrw';
-$user = 'cspchukc_tntrw';
-$password ='t0WcPRPN5EjiaEaw';
+	$dsn = 'mysql: host=localhost:3306; dbname=tnt';
+$user = 'tnt';
+$password ='**********';
 ?>
--- includes/smtp.php
+++ includes/smtp.php
@@ -1,5 +1,5 @@
 <?php
 $smtphost = "smtp.googlemail.com"; // SMTP Host Name
-$smtpuser = 'chuk.theater@gmail.com'; // SMTP User Name
-$smtppass = 'chuk.theater/001'; // SMTP password
+$smtpuser = 'info@tnt.rw'; // SMTP User Name
+$smtppass = '***************'; // SMTP password
 ?>
